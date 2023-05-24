695c442ceed272525e3c333954c19d1a25cb8f0b
Technologie31 <78386526+Technologie31@users.noreply.github.com>
2023-05-24 12:09:40 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,84 @@
+makerbit.onIrButton(IrButton.NUM8, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big2(0, 50, 50)
+})
+makerbit.onIrButton(IrButton.NUM6, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Pinkish)
+})
+makerbit.onIrButton(IrButton.NUM3, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Blue)
+})
+makerbit.onIrButton(IrButton.TLeft, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, vitesse)
+    basic.pause(100)
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.Light, IrButtonAction.Pressed, function () {
+    basic.showIcon(IconNames.Butterfly)
+})
+makerbit.onIrButton(IrButton.NUM9, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big2(100, 100, 0)
+})
+makerbit.onIrButton(IrButton.NUM7, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Yellow)
+})
+makerbit.onIrButton(IrButton.Right, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Right, vitesse)
+    basic.pause(100)
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.Left, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Left, vitesse)
+    basic.pause(100)
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.Minus, IrButtonAction.Pressed, function () {
+    vitesse += -10
+})
+makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, vitesse)
+    basic.pause(100)
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.NUM4, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big(Tinybit.enColor.White)
+})
+makerbit.onIrButton(IrButton.NUM2, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+})
+makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, vitesse)
+    basic.pause(100)
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.NUM5, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Cyan)
+})
+makerbit.onIrButton(IrButton.Power, IrButtonAction.Pressed, function () {
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+})
+makerbit.onIrButton(IrButton.NUM1, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+})
+makerbit.onIrButton(IrButton.NUM0, IrButtonAction.Pressed, function () {
+    Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
+})
+makerbit.onIrButton(IrButton.BEEP, IrButtonAction.Pressed, function () {
+    music.setVolume(70)
+    music.playTone(988, music.beat(BeatFraction.Sixteenth))
+    basic.pause(400)
+})
+makerbit.onIrButton(IrButton.TRight, IrButtonAction.Pressed, function () {
+    Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Right, vitesse)
+    basic.pause(100)
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.Plus, IrButtonAction.Pressed, function () {
+    vitesse += 10
+})
+let vitesse = 0
+vitesse = 80
+basic.showIcon(IconNames.House)
+makerbit.connectIrReceiver(DigitalPin.P8)
